931f820e5c9a3f91d7428a30ea728d64e65350d8
Pavel Kuznetsov <kuzn-25@mail.ru>
2024-11-26 23:11:14 +0300

    1.py

--- 1.py
+++ 1.py
@@ -1,1 +1,38 @@
-print('hello')
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+from aiogram import F
+
+bot = Bot(token='7729820925:AAFXfs801fLu875ChGvJeFSAeR3aqKDf4qQ')
+dp = Dispatcher()
+
+
+
+
+# Этот хэндлер будет срабатывать на команду "/start"
+@dp.message(Command(commands=["start"]))
+async def process_start_command(message: Message):
+    await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
+
+
+# Этот хэндлер будет срабатывать на команду "/help"
+@dp.message(Command(commands=['help', "puk"]))
+async def process_help_command(message: Message):
+    await message.answer('Напиши мне что-нибудь и в ответ '
+                         'я пришлю тебе твое сообщение')
+
+@dp.message(F.photo)
+async def send_photo_echo(message: Message):
+    await message.reply_photo(message.photo[0].file_id)
+
+# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
+# кроме команд "/start" и "/help"
+
+@dp.message()
+async def send_echo(message: Message):
+    await message.reply(text=message.text)
+
+dp.message.register(send_echo)
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
